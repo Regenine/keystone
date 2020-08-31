1a09805a0b27d992e9b6dd31f974db06101b3a11
Tim Leslie <timl@thinkmill.com.au>
2020-08-31 13:38:08 +1000

    Consolidate field filter tests

--- packages/fields-unsplash/src/test-fixtures.js
+++ packages/fields-unsplash/src/test-fixtures.js
@@ -1,11 +1,12 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 import { getItems } from '@keystonejs/server-side-graphql-client';
+import { Text } from '@keystonejs/fields';
 import { Unsplash } from './';
 
 // Field configurations
 export const name = 'Unsplash';
-export { Unsplash as type };
+export const type = Unsplash;
 export const supportsUnique = false;
 export const skipRequiredTest = false;
 export const exampleValue = 'U0tBTn8UR8I';
@@ -13,29 +14,42 @@ export const exampleValue2 = 'xrVDYZRGdw4';
 export const fieldName = 'heroImage';
 export const subfieldName = 'unsplashId';
 export const fieldConfig = {
-  accessKey: process.env.UNSPLASH_KEY,
-  secretKey: process.env.UNSPLASH_SECRET,
+  accessKey: process.env.UNSPLASH_KEY || 'unsplash_key',
+  secretKey: process.env.UNSPLASH_SECRET || 'unplash_secret',
 };
 
-export const getTestFields = () => {
-  return {
-    name: { type: String },
-    heroImage: {
-      type: Unsplash,
-      accessKey: process.env.UNSPLASH_KEY,
-      secretKey: process.env.UNSPLASH_SECRET,
-    },
-  };
-};
+export const getTestFields = () => ({
+  name: { type: Text },
+  heroImage: {
+    type,
+    accessKey: process.env.UNSPLASH_KEY || 'unsplash_key',
+    secretKey: process.env.UNSPLASH_SECRET || 'unplash_secret',
+  },
+});
 
 export const initItems = () => {
   return [
     { name: 'a', heroImage: 'dYEuFB8KQJk' },
     { name: 'b', heroImage: '95YRwf6CNw8' },
-    { name: 'c', heroImage: null },
-    { name: 'd' },
+    { name: 'c', heroImage: 'U0tBTn8UR8I' },
+    { name: 'd', heroImage: 'xrVDYZRGdw4' },
+    { name: 'e', heroImage: 'dYEuFB8KQJk' },
+    { name: 'f', heroImage: null },
+    { name: 'g' },
   ];
 };
+
+export const storedValues = () => [
+  { name: 'a', heroImage: { unsplashId: 'dYEuFB8KQJk' } },
+  { name: 'b', heroImage: { unsplashId: '95YRwf6CNw8' } },
+  { name: 'c', heroImage: { unsplashId: 'U0tBTn8UR8I' } },
+  { name: 'd', heroImage: { unsplashId: 'xrVDYZRGdw4' } },
+  { name: 'e', heroImage: { unsplashId: 'dYEuFB8KQJk' } },
+  { name: 'f', heroImage: null },
+  { name: 'g', heroImage: null },
+];
+
+export const supportedFilters = ['null_equality', 'in_empty_null'];
 
 export const filterTests = withKeystone => {
   const match = async (keystone, where, expected, sortBy = 'name_ASC') =>
